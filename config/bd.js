c341c9898fb73eee0e809a9e380688348a71edce
Edinson Antolinez <edinson.antolinez@tus-cuentas.com>
2020-04-12 11:33:52 -0500

    app deploy

--- config/bd.js
+++ config/bd.js
@@ -1,20 +1,14 @@
 'use strict';
 
 var mysql = require('promise-mysql');
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 
 exports.getConnection = () => {
-	return mysql.createConnection({
-		host: config.host,
-		user: config.username,
-		password: config.password,
-		database: config.database
-	})
+    return mysql.createConnection({
+        host: config.host,
+        user: config.username,
+        password: config.password,
+        database: config.database
+    })
 }
-
-
-
-
-
-
--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 var bd = require('./config/bd')
 
 var app = require('./app');
-var port = 4200
+const PORT = process.env.PORT || 8080;
 
-app.listen(port, () => {
-    console.log('Server running')
-    // bd.connection()
+app.listen(PORT, () => {
+    console.log('SERVIDOR CORRIENDO EN EL PUERTO ', PORT)
+        // bd.connection()
 })
-
